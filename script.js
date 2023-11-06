be12f1c5e46a1df3066a3c5b9542fb63a0f0a738
Vishnupalatt <vishnupalattuparambil123@gmail.com>
2023-11-06 09:08:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function display(val){
+    result.value+=val
+}
+function evalExp(){
+    res=eval(result.value)
+    result.value=res
+}
+function allClear(){
+    result.value=''
+}
+function bspace(){
+    result.value=result.value.slice(0,-1)
+}
+function percentage(){
+    res=(result.value)/100
+    result.value=res
+}
